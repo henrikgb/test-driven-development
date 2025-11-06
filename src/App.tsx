f99f69e0c6600e21cf8567a6ca78a103b05c4007
henrikgb <henrik-gb@hotmail.com>
2025-11-06 10:45:09 +0100

    :recycle: Update text on landing page

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          This App is intended to practice Test Driven Development (TDD) with Typescript and Jest.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
